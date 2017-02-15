038b599bb337860fd71e6478dc4a755a2117fc49
Tom Zellman <tzellman@gmail.com>
2017-02-14 21:17:39 -0500

    WIP

--- app/pods/tasks/controller.js
+++ app/pods/tasks/controller.js
@@ -3,74 +3,96 @@ const {Controller, computed} = Ember;
 
 export default Controller.extend({
 
-    focusData: computed("model", function () {
+    focusCharts: computed("model", function () {
+
+        const sortByTime = (d1, d2) => d1.name === 'Control' ? -1 : (d1.y < d2.y ? -1 : 1);
+        const sortByPercent = (d1, d2) => d1.name === 'Control' ? -1 : (d1.y < d2.y ? 1 : -1);
 
         const data = this.get("model");
         const timeSeries = {
             name: "Time",
             type: 'column',
-            yAxis: 0,
-            data: data.map(d => d.focusTime)
+            data: data.map(d => ({y: d.focusTime, name: d.label})).sort(sortByTime)
         };
 
         const percent2 = {
             name: "Percent Correct with 2 distractions",
             type: 'column',
-            yAxis: 1,
-            data: data.map(d => d.percent2)
+            data: data.map(d => ({y: d.percent2, name: d.label})).sort(sortByPercent)
         };
 
         const percent6 = {
             name: "Percent Correct with 6 distractions",
             type: 'column',
-            yAxis: 1,
-            data: data.map(d => d.percent6)
+            data: data.map(d => ({y: d.percent6, name: d.label})).sort(sortByPercent)
         };
 
-        return [timeSeries, percent2, percent6];
+        return [
+            {
+                series: [timeSeries],
+                title: "Focus : Total Time",
+                yMin: 0,
+                yTitle: "Time (seconds)"
+            },
+            {
+                series: [percent2],
+                title: "Percent Correct with 2 Distractions",
+                yMin: 0,
+                yMax: 100,
+                yTitle: "Percent Correct"
+            },
+            {
+                series: [percent6],
+                title: "Percent Correct with 6 Distractions",
+                yMin: 0,
+                yMax: 100,
+                yTitle: "Percent Correct"
+            }
+        ];
     }),
 
-    juggleData: computed("model", function () {
+    sortedData: computed("model", function () {
+        const sortFunc = (d1,
+                          d2) => d1.name === 'Control' ? -1 : (d2.name === 'Control' ? 1 : ((d1.switching + d1.repeating) < (d2.switching + d2.repeating) ? -1 : 1));
+        return this.get("model").sort(sortFunc);
+    }),
 
-        const data = this.get("model");
+    juggleData: computed("sortedData", function () {
+
+        const data = this.get("sortedData");
 
         const switching = {
             name: "Switching between tasks",
             type: 'column',
             yAxis: 0,
-            data: data.map(d => d.switching / 1000.)
+            data: data.map(d => d.switching / 1000.0)
         };
 
         const repeating = {
             name: "Repeating the same task",
             type: 'column',
             yAxis: 0,
-            data: data.map(d => d.repeating / 1000.)
+            data: data.map(d => d.repeating / 1000.0)
         };
 
         return [switching, repeating];
     }),
 
-    categories: computed("model", function () {
-        return this.get("model").map(d => d.label);
+    categories: computed("sortedData", function () {
+        return this.get("sortedData").map(d => d.label);
     }),
 
-    focusYAxis: computed(function () {
-        return [{ // time axis
-            title: {
-                text: 'Time (seconds)',
-            }
-        }, { // percent axis
+    percentAxis: computed(function () {
+        return [{ // percent axis
             title: {
                 text: 'Percent Correct',
             },
-            opposite: true,
             min: 0,
             max: 100
         }];
     }),
 
-    juggleYAxis: computed(function () {
+    timeAxis: computed(function () {
         return [{ // time axis
             title: {
                 text: 'Time (seconds)',
